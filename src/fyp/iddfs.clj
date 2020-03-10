76218e6e41ef4b0851b506a7d154ca13d68075f5
Jason <t7047098@live.tees.ac.uk>
2020-03-10 20:17:47 +0000

    updated version of iddfs

--- src/fyp/iddfs.clj
+++ src/fyp/iddfs.clj
@@ -8,7 +8,7 @@
   [v coll]
   (some #(= % v) coll))
 
-(defn dls
+(defn dls1
   [graph start goal depth]
   (cond
     (= depth 0)
@@ -32,18 +32,37 @@
               (let [node (peek stack)
                     neighbours (-> (find-neighbours node graph) reverse)
                     not-visited (filter (complement #(visited? % visited)) neighbours)
-                    new-stack (into (if (= depth-gone 0)
-                                      stack
-                                      (pop stack))
-                                    not-visited)]
+                    new-stack (into (if (= depth-gone 0) stack (pop stack)) not-visited)]
                 (if (visited? node visited)
                   (recur new-stack visited (+ depth-gone 1))
-                  (recur
-                      new-stack
-                    (conj visited node)
-                    (if (empty? not-visited)
-                      (- depth-gone 1)
-                      (+ depth-gone 1)))))))))))
+                  (recur new-stack (conj visited node) (if (empty? not-visited) (- depth-gone 1) (+ depth-gone 1)))))))))))
+
+(defn dls
+  [graph start goal depth]
+  (cond
+    (= depth 0)
+      (if (= goal start)
+        start
+        [])
+    :else
+    (loop [stack (vector start)
+          visited []
+           depth-gone 0]
+     (if (empty? stack)
+       []
+       (if (= goal (peek stack))
+         (conj visited (peek stack))
+         (let [node (peek stack)
+               neighbours (-> (find-neighbours node graph) reverse)
+               not-visited (filter (complement #(visited? % visited)) neighbours)
+               new-stack (into (pop stack) (if (= depth depth-gone) nil not-visited))
+               new-depth (cond
+                           (= depth-gone (count stack)) (count stack)
+                           (and (= depth (count stack)) (= (- depth depth-gone) 1)) (+ depth-gone 1)
+                           (> (count stack) depth-gone) depth-gone
+                           :else (+ depth-gone 1))]
+             (recur new-stack (conj visited node) new-depth))
+             )))))
 
 (defn iddfs
   [graph start goal]
@@ -56,3 +75,11 @@
         (recur (dls graph start goal (+ depth 1)) (+ depth 1))
         :else
         "No Solution"))))
+
+(def tree {:A [:B :E]
+           :B [:C :D]
+           :E [:F :G]
+           :C [:H :I]
+           :G [:J :K]})
+
+(iddfs tree :A :K)
