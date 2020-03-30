cc980da5a0447340143c8cfa728ef1016919add3
Jason <t7047098@live.tees.ac.uk>
2020-03-30 13:26:48 +0100

    Removed code that was for self testing purposes

--- src/fyp/dijkstra.clj
+++ src/fyp/dijkstra.clj
@@ -37,7 +37,3 @@
      start (keyword sta)
      goal (keyword go)]
     (Dijkstra-method start goal graph)))
-
-(time (-dijkstraMethod "A"
-                    "C"
-                    ":A [{:B 20 :E 10 :D 4 :G 9 :J 15}]\n :B [{:A 20 :C 11 :D 9 :E 21}]\n :C [{:B 11 :H 13 :I 14 :D 3}]\n :D [{:B 9 :A 4 :C 3 :H 15 :J 10}]\n :E [{:A 10 :B 21 :F 16 :G 17}]\n :F [{:E 16 :G 24}]\n :G [{:E 17 :F 24 :K 5 :J 7 :A 9}]\n :H [{:C 13 :I 2 :D 15 :J 4 :K 23}]\n :I [{:H 2 :C 14}]\n :J [{:G 7 :K 10 :H 4 :D 10 :A 15}]\n :K [{:G 5 :J 10 :H 23}]"))
